4477861c5b0856b41e78a9efe24b32c1ea7ee5cb
Aung KO <aungko.uit@gmail.com>
2022-12-14 12:31:48 +0630

    add listening port

---
+++ server.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var index_1 = require("./index"); //ask why I can't use app var_name again
+index_1["default"].listen("8000", function () { return console.log("Server is running at port 8000"); });
---
+++ server.ts
@@ -0,0 +1,2 @@
+import app from './index'; //ask why I can't use app var_name again
+app.listen("8000", () => console.log("Server is running at port 8000"))
