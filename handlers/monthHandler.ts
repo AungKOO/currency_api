2ad12a9a285fc3a886400ab9b0309da8c824ff1b
Aung KO <aungko.uit@gmail.com>
2022-12-20 12:25:10 +0630

    style: Adding try catch and data validation

--- handlers/monthHandler.ts
+++ handlers/monthHandler.ts
@@ -2,12 +2,13 @@ import getMonthlyRate from "../servcies/month";
 
 export default async (req: any, res: any) => {
   console.log(req.params.id);
-  // try {
-  let data = await getMonthlyRate();
-  console.log("data: ", data);
-  return res.status(200).json(data);
-  // } catch (err) {
-  //   console.log(err);
-  //   return res.status(500);
-  // }
+  const id = req.params.id;
+  try {
+    let data = await getMonthlyRate(id);
+
+    return res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(400);
+  }
 };
--- servcies/month.ts
+++ servcies/month.ts
@@ -1,8 +1,16 @@
 import * as fs from "fs/promises"
 
-async function getMonthlyRate() {
-    return fs.readFile("month.json")
-    .then(data => JSON.parse(data.toString()));
+async function getMonthlyRate(id: string) {
+
+    const month = id.substring(4,6)  // * get month 
+    console.log(`month: ${month} ${id}`)
+    if (parseInt(month) === 11) {
+        return fs.readFile("month.json")
+    .then(data => JSON.parse(data.toString())).catch(e => console.log(e));
+    }
+    else {
+        return Promise.reject("invalid month")
+    }
 }
 export default getMonthlyRate;
 
